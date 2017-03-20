f9bea1db6b57ec1b9c04642316862238b7361571
puz_ya <puzino.y.a@gmail.com>
2017-03-21 00:56:25 +0300

    Added menu, onActivityResult (to check login); Fixed view crash.

--- app/src/main/java/com/py/firebasechat/MainActivity.java
+++ app/src/main/java/com/py/firebasechat/MainActivity.java
@@ -1,9 +1,14 @@
 package com.py.firebasechat;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.Snackbar;
 import android.text.format.DateFormat;
 import android.support.constraint.ConstraintLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -12,6 +17,8 @@ import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.FirebaseDatabase;
 
@@ -66,9 +73,9 @@ public class MainActivity extends AppCompatActivity {
             @Override
             protected void populateView(View v, Message model, int position) {
                 TextView message, author, time;
-                message = (TextView) findViewById(R.id.tvMessage);
-                author = (TextView) findViewById(R.id.tvUser);
-                time = (TextView) findViewById(R.id.tvTime);
+                message = (TextView) v.findViewById(R.id.tvMessage);
+                author = (TextView) v.findViewById(R.id.tvUser);
+                time = (TextView) v.findViewById(R.id.tvTime);
 
                 message.setText(model.getMessageText());
                 author.setText(model.getMessageAuthor());
@@ -76,5 +83,44 @@ public class MainActivity extends AppCompatActivity {
                 time.setText(DateFormat.format("dd-MM-yyyy (HH:mm)", model.getMessageTime()));
             }
         };
+
+        listView.setAdapter(adapter);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == SIGN_IN_REQUEST){
+            if(resultCode == RESULT_OK){
+                Snackbar.make(activity_main, "Log in success", Snackbar.LENGTH_SHORT).show();
+                displayChat();
+            } else {
+                Snackbar.make(activity_main, "Log in failed", Snackbar.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if(item.getItemId() == R.id.menu_logout){
+            //log out current user
+            AuthUI.getInstance().signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            Snackbar.make(activity_main, "Log out success", Snackbar.LENGTH_SHORT).show();
+                            finish();
+                        }
+                    });
+        }
+        return true;
     }
 }
